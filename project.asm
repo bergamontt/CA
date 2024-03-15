15a3f9c3d07a05d2cb9e117e1dca574f8b253443
Daria <da.bulavina@ukma.edu.ua>
2024-03-15 16:23:34 +0200

    - changed input from file to console read - added arrays that will hold users input

--- project.asm
+++ project.asm
@@ -6,50 +6,59 @@
 ;----- block of variables
 
 .data
-
-    file_name db 'test.in', 0      ; insert file name here before starting the program
-    buffer_size equ 256
-    buffer db buffer_size dup(0)
-    error db 'Error has ocurred while opening the file$', 0
+    ;keys db 10000 * 17 DUP(?)  
+    ;value db 10000 * 16 DUP(?)  
+    temp_key db 8 dup(?)
+    temp_value db 8 dup(?)
+    prompt db 0dh,0ah,"Enter your string(7 Chars Max): $"
+    msg1 db  0dh,0ah,"Your input: $"
+    sev db 7 dup(?)
     
 .code
-
 main PROC
 
     ;----- initialization
 
-     mov ax, @data      ; Set ax to data segment
-     mov es, ax        
+        mov ax, @data
+        mov ds, ax
+        mov es, ax
+        
+    ;----- clear screen
 
+        mov al, 03h
+        mov ah, 00h
+        int 10h   
 
-    ;----- attempt to open the file
+    ;----- prep
 
-    mov ah, 3Dh         ; DOS function for open file
-    lea dx, file_name   ; pointer to the file name
-    mov al, 0           ; access mode read-only
-    int 21h             ; syscall (opening the file)
+        mov dx, offset prompt
+        mov ah,09h
+        int 21h
+        lea si, temp_key
+        mov cx,16
 
-    ;----- read byte
-    
-    read_next:
-    mov ah, 3Fh
-    mov bx, 0h  ; stdin handle
-    mov cx, 1   ; 1 byte to read
-    mov dx, offset file_name   ; read to ds:dx 
-    int 21h   ;  ax = number of bytes read
+    ;----- read row from console
 
-    ;----- print to console
-    mov ah, 02h
-    mov dl, al
-    int 21h
+        read_row:
+        mov ah,01
+        int 21h
+        mov [si],al
+        inc si
+        loop read_row
 
-    ;----- loop
-    or ax,ax
-    jnz read_next
+    ;----- print input
+
+        mov si+sev,'$'
+        mov dx,offset msg1
+        mov ah,09h
+        int 21h
+        lea dx,temp_key
+        mov ah,09h
+        int 21h
 
     ;----- termination of a program
-    mov ax, 4C00h ; Terminate program
-    int 21h
+        mov ah, 4ch
+        int 21h
 
 main ENDP
 END main
