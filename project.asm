e19528befb158208f9d51c2514fbf209ec80b9cd
Daria <da.bulavina@ukma.edu.ua>
2024-03-17 17:09:57 +0200

    - user's temp line is now divided into a key and value -removed .gitignore

--- project.asm
+++ project.asm
@@ -6,11 +6,18 @@
 ;----- block of variables
 
 .data
-    ;keys db 10000 * 17 DUP(?)  
-    ;value db 10000 * 16 DUP(?) 
+    keys db 1000 dup (?) 
+    values db 1000 dup (?)
+
+    keys_size db 6
+    value_size db 16 
+
     ROW db 23 dup (?) 
-    temp_key db 8 dup(?)
-    temp_value db 8 dup(?)
+    ROWS db 1000 dup (?)
+    
+    temp_key db 16 dup(?)
+    temp_value db 6 dup(?)
+
     
 .code
 main PROC
@@ -48,7 +55,39 @@ main PROC
         mov bx, 0
         jmp write_row
 
-    ;----- read row from console
+    ;----- split row to key and value
+
+        split:
+        mov bx, 0
+        mov bp, 0
+
+    ;----- read key to temp storage
+
+        read_key:
+        cmp [ROW + bx], 32
+        je read_value
+        mov dl, [ROW + bx]
+        mov [temp_key + bp], dl
+        inc bx
+        inc bp
+        jmp read_key
+
+    ;----- read value to temp storage
+
+        read_value:
+        inc bx
+        cmp [ROW + bx], 13
+        je add_to_array
+        mov dl, [ROW + bx]
+        mov [temp_value + bp], dl
+        inc bp
+        jmp read_value
+
+    ;----- add key and values to arrays
+
+        add_to_array:
+
+    ;----- write row to console
 
         write_row:
         mov ah, 02h
