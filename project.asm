b20d82d51fa6d9708f96f45fbfddd95159c501c9
Daria <da.bulavina@ukma.edu.ua>
2024-03-20 13:01:54 +0200

    - fixed a problem with negative numbers during their convertion

--- project.asm
+++ project.asm
@@ -7,7 +7,7 @@
 
 .data
     keys db 1000 dup (?) 
-    values db 1000 dup (?)
+    values dw 1000 dup (?)
 
     keys_size db 6
     value_size db 16 
@@ -24,6 +24,8 @@
 
     num_of_keys dw 0
     num_of_values dw 0
+
+    temp_buffer dw 0
     
 .code
 main PROC
@@ -69,6 +71,7 @@ main PROC
 
     ;----- read key to temp storage
 
+        mov temp_key_length, 0
         read_key:
         cmp [ROW + bx], 32
         je read_value
@@ -76,10 +79,12 @@ main PROC
         mov [temp_key + bp], dl
         inc bx
         inc bp
+        inc temp_key_length
         jmp read_key
 
     ;----- read value to temp storage
 
+        mov temp_value_length, 0
         read_value:
         inc bx
         cmp [ROW + bx], 13
@@ -87,6 +92,7 @@ main PROC
         mov dl, [ROW + bx]
         mov [temp_value + bp], dl
         inc bp
+        inc temp_value_length
         jmp read_value
 
     ;----- add key to array with keys
@@ -115,9 +121,11 @@ main PROC
         jmp check_sign
         
         add_value_to_array:
+        mov bx, num_of_keys
+        mov ax, temp_key
+        mov [values + bx], ax
+        inc num_of_keys
 
-        ;turn ascii into decimal
-        ;store it as hex number in array
         ;jump back to get next line from user
 
     ;----- check if value is negative
@@ -132,21 +140,55 @@ main PROC
     ;----- convert from ascii to hex for positive number
 
         positive_number:
+
+        mov ax, 0
+        mov dx, 0
         mov bx, temp_key_length
+        mov cl, 1
         positive_loop:
         dec bx
         mov al, [temp_key + bx]
-        mov cl, 10
+        sub al, 48
         mul cx
-        add dx, ax
-        cmp bx, 0
+        add temp_buffer, ax
+        mov dx, temp_buffer
 
+        mov ax, 10
+        mul cx
+        mov cx, ax
+
+        cmp bx, 0
         ja positive_loop
+        mov ax, temp_buffer
         jmp add_value_to_array
 
     ;----- convert from ascii to hex for negative number
 
         negative_number:
+        mov ax, 0
+        mov dx, 0
+        mov bx, temp_key_length
+        mov cl, 1
+        negative_loop:
+        dec bx
+        mov al, [temp_key + bx]
+        sub al, 48
+        mul cx
+        add temp_buffer, ax
+        mov dx, temp_buffer
+
+        mov ax, 10
+        mul cx
+        mov cx, ax
+
+        cmp bx, 1
+        ja negative_loop
+        mov ax, temp_buffer
+        xor al, 255
+        xor ah, 255
+        add ax, 1
+        mov temp_buffer, ax
+        jmp add_value_to_array
 
     ;----- write row to console
 
