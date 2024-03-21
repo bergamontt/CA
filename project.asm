5f9488128435c056881403cc4fc34f9575de761d
Daria <da.bulavina@ukma.edu.ua>
2024-03-21 23:53:35 +0200

    - change byte reading from 01h to 3fh, now this program reads the file until eof - changed output/input and the way data is stored

--- project.asm
+++ project.asm
@@ -2,208 +2,123 @@
 ;      Variant 3
 
 .model small
-
-;----- block of variables
+.stack 100h
 
 .data
-    keys db 1000 dup (?) 
-    values dw 1000 dup (?)
 
-    keys_size db 6
-    value_size db 16 
+KEYS db 1000 dup (?)
+VALUES db 1000 dup (?)
+H_VALUES dw 1000 dup (?)
+DUMP db 23000 dup (?)
 
-    ROW db 23 dup (?) 
-    ROWS db 1000 dup (?)
-    
-    temp_key db 16 dup(?)
-    temp_value db 6 dup(?)
-    temp_key_length dw 0
-    temp_value_length dw 0
+BYTES_READ dw 0;
+BYTES_CHECKED dw 0;
+NUM_OF_ROWS dw 0;
 
-    is_Negative db 0
+LAST_INDEX dw 0;
+NUM_OF_KEYS dw 0
 
-    num_of_keys dw 0
-    num_of_values dw 0
+KEY_SIZE db 16
+VALUE_SIZE db 6
 
-    temp_buffer dw 0
-    
 .code
-main PROC
+start PROC
 
-    ;----- initialization
+    mov ax, @data
+    mov ds, ax
 
-        mov ax, @data
-        mov ds, ax
-        mov es, ax
-        mov bx, 0
-        
-    ;----- clear screen
+read_next:
 
-        mov al, 03h
-        mov ah, 00h
-        int 10h   
+    mov ah, 3Fh       
+    mov bx, 0         
+    mov cx, 23000       
+    mov dx, offset [DUMP] 
+    int 21h         
+    mov BYTES_READ, ax
+    jmp init_arrays   
 
-    ;----- read row from console
+init_arrays:
 
-        read_row:
-        mov ah,01
-        int 21h
+    mov bx, 16
+    mov ax, NUM_OF_KEYS
+    mul bx
+    mov cx, ax
+    jmp fill_key
 
-        cmp al, 13
-        je string_end
+    fill_key:
 
-        mov [ROW + bx],al
-        cmp bx, 23
-        inc bx
-        jbe read_row
-        mov bx, 0
-        jmp write_row
+        mov bx, LAST_INDEX
 
-        string_end:
-        mov bx, 0
-        jmp write_row
+        mov al, [DUMP + bx]
+        cmp al, 32
+        je setup_value
 
-    ;----- split row to key and value
+        mov bx, cx
+        mov [KEYS + bx], al
 
-        split:
-        mov bx, 0
-        mov bp, 0
+        inc cx
+        inc LAST_INDEX
+        inc BYTES_CHECKED
 
-    ;----- read key to temp storage
+        jmp fill_key
 
-        mov temp_key_length, 0
-        read_key:
-        cmp [ROW + bx], 32
-        je read_value
-        mov dl, [ROW + bx]
-        mov [temp_key + bp], dl
-        inc bx
-        inc bp
-        inc temp_key_length
-        jmp read_key
 
-    ;----- read value to temp storage
+    setup_value:
 
-        mov temp_value_length, 0
-        read_value:
-        inc bx
-        cmp [ROW + bx], 13
-        je add_to_array
-        mov dl, [ROW + bx]
-        mov [temp_value + bp], dl
-        inc bp
-        inc temp_value_length
-        jmp read_value
-
-    ;----- add key to array with keys
-
-        add_to_array:
-
-        add_key:
-        mov ax, num_of_keys
-        mov bl, keys_size
+        inc BYTES_CHECKED
+        inc LAST_INDEX
+        mov ax, NUM_OF_KEYS
+        mov bx, 6
         mul bx
-        mov bx, ax
-        mov bp, 0
+        mov cx, ax
+        jmp fill_value
 
-        add_key_bytes:
-        mov al, [temp_key + bp]
-        mov [keys + bx], al
-        cmp bp, temp_key_length
-        inc bp
-        jbe add_key_bytes
-        jmp add_value
+        fill_value:
 
-    ;----- add value to array with values
+            mov bx, LAST_INDEX
+            mov al, [DUMP + bx]
 
-        add_value:
-        mov dx, 0
-        jmp check_sign
+            cmp al, 13
+            je prep_for_loop
+            cmp al, 10
+            je prep_for_loop
+
+            mov bx, cx
+            mov [VALUES + bx], al
+
+            inc cx
+            inc LAST_INDEX
+            inc BYTES_CHECKED
+
+            jmp fill_value
         
-        add_value_to_array:
-        mov bx, num_of_keys
-        mov ax, temp_key
-        mov [values + bx], ax
-        inc num_of_keys
+        prep_for_loop:
 
-        ;jump back to get next line from user
+            inc LAST_INDEX
+            inc NUM_OF_KEYS
+            inc BYTES_CHECKED
+            inc LAST_INDEX
+            inc BYTES_CHECKED
 
-    ;----- check if value is negative
+            mov cx, BYTES_READ
+            cmp BYTES_CHECKED, cx
+            jae end_program
+            jmp init_arrays
+    
+end_program:
 
-        check_sign:
+    mov bx, 0
+    write:
+    mov dl, [VALUES + bx]
+    mov ah, 02h
+    int 21h
+    inc bx
+    cmp bx, 7
+    jne write
 
-        mov ax, 0
-        cmp [temp_value], '-'
-        je negative_number
-        jmp positive_number
+    mov ax, 4C00h   
+    int 21h          
 
-    ;----- convert from ascii to hex for positive number
+start ENDP
 
-        positive_number:
-
-        mov ax, 0
-        mov dx, 0
-        mov bx, temp_key_length
-        mov cl, 1
-        positive_loop:
-        dec bx
-        mov al, [temp_key + bx]
-        sub al, 48
-        mul cx
-        add temp_buffer, ax
-        mov dx, temp_buffer
-
-        mov ax, 10
-        mul cx
-        mov cx, ax
-
-        cmp bx, 0
-        ja positive_loop
-        mov ax, temp_buffer
-        jmp add_value_to_array
-
-    ;----- convert from ascii to hex for negative number
-
-        negative_number:
-        mov ax, 0
-        mov dx, 0
-        mov bx, temp_key_length
-        mov cl, 1
-        negative_loop:
-        dec bx
-        mov al, [temp_key + bx]
-        sub al, 48
-        mul cx
-        add temp_buffer, ax
-        mov dx, temp_buffer
-
-        mov ax, 10
-        mul cx
-        mov cx, ax
-
-        cmp bx, 1
-        ja negative_loop
-        mov ax, temp_buffer
-        xor al, 255
-        xor ah, 255
-        add ax, 1
-        mov temp_buffer, ax
-        jmp add_value_to_array
-
-    ;----- write row to console
-
-        write_row:
-        mov ah, 02h
-        mov dx, offset [ROW + bx]
-        int 21h
-
-        cmp bx, 23
-        inc bx
-        jbe write_row
-
-    ;----- termination of a program
-        mov ah, 4ch
-        int 21h
-
-main ENDP
-END main
+END start
